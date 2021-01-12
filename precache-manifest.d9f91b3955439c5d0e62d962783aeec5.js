c75bae85f9ac01453571859e6d339f8998b7b088
adonisShareef <adonis_shareef@hotmail.com>
2021-01-12 00:06:40 -0800

    deploy

---
+++ precache-manifest.d9f91b3955439c5d0e62d962783aeec5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21256e8448ed1fdfcdeb",
+    "url": "/css/about.a31a932b.css"
+  },
+  {
+    "revision": "911a083985c44128404e",
+    "url": "/css/app.89a85e65.css"
+  },
+  {
+    "revision": "84817b8b6ede3134d4ab",
+    "url": "/css/chunk-vendors.367538c2.css"
+  },
+  {
+    "revision": "8b467cd9d74c5085d6df",
+    "url": "/css/contact.7bf40cd4.css"
+  },
+  {
+    "revision": "be0f07def2738ddf438b",
+    "url": "/css/discography.fa97d4ad.css"
+  },
+  {
+    "revision": "15db5239561b69567173dfde076a2316",
+    "url": "/img/RahsaanHakeemphoto.15db5239.jpg"
+  },
+  {
+    "revision": "4612aa36e431b1daf0988afa31a0cc52",
+    "url": "/img/RandH.4612aa36.png"
+  },
+  {
+    "revision": "fcc83100245e235f87dfce5660a1611f",
+    "url": "/img/applemusic-icon.fcc83100.png"
+  },
+  {
+    "revision": "866a94fe06092efbff527d0290cc83b2",
+    "url": "/img/darling_white.866a94fe.png"
+  },
+  {
+    "revision": "6529141e25c4f192cf31e5be826b11ce",
+    "url": "/img/facebook-icon.6529141e.png"
+  },
+  {
+    "revision": "fa47a1ff9dfbf0574a087831a0d2f7c5",
+    "url": "/img/instagram-icon.fa47a1ff.png"
+  },
+  {
+    "revision": "f252b06c12719cf707d13a3ad857ad27",
+    "url": "/img/time-to-heal.f252b06c.png"
+  },
+  {
+    "revision": "dcbd010792cc0b08d563b4703c8eb238",
+    "url": "/img/twitter-icon.dcbd0107.png"
+  },
+  {
+    "revision": "69069481441bb4dfbdb2ca84e810f1dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21256e8448ed1fdfcdeb",
+    "url": "/js/about.d5f75d5d.js"
+  },
+  {
+    "revision": "911a083985c44128404e",
+    "url": "/js/app.6d9983dd.js"
+  },
+  {
+    "revision": "84817b8b6ede3134d4ab",
+    "url": "/js/chunk-vendors.b225595b.js"
+  },
+  {
+    "revision": "8b467cd9d74c5085d6df",
+    "url": "/js/contact.4e5045f3.js"
+  },
+  {
+    "revision": "be0f07def2738ddf438b",
+    "url": "/js/discography.ac1f86bd.js"
+  },
+  {
+    "revision": "db31bf4807f47c668280a84a26a5bfcf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
